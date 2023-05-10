c0b82d81ae3c8a12ad030e0e029c7430058514fc
Your Name <you@example.com>
2023-05-10 14:59:22 -0400

    =

---
+++ README.md
@@ -0,0 +1,16 @@
+### Arikaim CMS html widget library 
+![version: 1.0.0](https://github.com/arikaim/widget-library.git)
+![license: GPL3](https://img.shields.io/badge/License-GPLv3-blue.svg)
+
+  
+#### Requirements 
+  * [Arikaim CMS](https://github.com/arikaim/arikaim)
+  
+
+#### Installation
+
+```sh
+
+composer require arikaim/widget-library
+
+```
--- arikaim-widget.js
+++ arikaim-widget.js
@@ -7,7 +7,7 @@
 'use strict';
 
 (function() {
-    var ARIKAIM_WIDGET_VERSION = '0.5.1';
+    var ARIKAIM_WIDGET_VERSION = '1.0.0';
 
     function loadScript(url, onLoad, async) {
         var script = document.createElement('script');
@@ -22,74 +22,89 @@
         (document.getElementsByTagName('head')[0] || document.documentElement).appendChild(script);     
     }
 
-    function loadCss(url) {
-
-    };
-
-    function request(method, url, onSuccess, onError) {
-        $.ajax({
-            url: url,
-            method: method,          
-            data: {},
-            crossDomain: true,
-            beforeSend: function(request) {
-                request.setRequestHeader('Accept','application/json; charset=utf-8');
-            },
-            success: function(data) {   
-            },
-            error: function(xhr,status,error) {
-            }
-        });   
-    }
-
     console.log('Arikaim Widget version ' + ARIKAIM_WIDGET_VERSION);
 
     var currentScript = document.currentScript;
     var url = new URL(currentScript.src);
 
-    console.log(url);
-
+    var BASE_URL = url.href.split('/')[3];
     var LIBRARY_URL = url.href.replace('widget/arikaim-widget.js','');
     var WIDGET_UUID = currentScript.getAttribute('widget-container');
     var CONTANTER_CLASS = "widget-container-" + WIDGET_UUID;
     var ARIKAIM_HOST = url.origin;
+    var REMOTE_URL = ARIKAIM_HOST + '/' + BASE_URL;
    // var jQuery, $;
 
-    console.log(WIDGET_UUID);
-    console.log(CONTANTER_CLASS);
-    console.log(LIBRARY_URL);
+   // console.log(ARIKAIM_HOST);    
+   // console.log(CONTANTER_CLASS);
+   // console.log(LIBRARY_URL);
  
-
     // Load Jquery
     loadScript(LIBRARY_URL + 'jquery/jquery-3.6.1.min.js',function() {
        // $ = jQuery = window.jQuery.noConflict(true);      
         console.log('jQuery loaded.');
         loadScript(LIBRARY_URL + 'arikaim/arikaim.js',function() {
             console.log('Arikaim lib loaded.');
-
             arikaim.setHost(ARIKAIM_HOST);
-            arikaim.setBaseUrl('/arikaim');
-            loadWidget();
-        });
-        // Call widget api include files 
-        //request(WIDGET_API_URL,function(result) {
-
-        //},funciton(error) {
-
-       // });
+            arikaim.setBaseUrl(ARIKAIM_HOST + '/ai');
+           
+            loadWidget();          
+        });       
     });
 
     function loadWidget() {
         // include files
-        arikaim.get('/api/widgets/includes/' + WIDGET_UUID,function(result) {
-
-            result.js.forEach(item => {
-                console.log(item);
-            });
-           
-        });
-        // fetch content
+        arikaim.get('/api/widgets/component/' + WIDGET_UUID,function(result) {
         
+            loadWidgetIncludeFiles(result.include.js,function() {
+                loadWidgetFiles(result.component.js,function() {
+                    // load css file
+
+                    console.log(result.component);
+                    console.log(REMOTE_URL + '/arikaim/view/templates/blog/components/widget/widget.css');
+
+                    arikaim.includeCSSFile(REMOTE_URL + '/arikaim/view/templates/blog/components/widget/widget.css')
+                    console.log('Widget loaded.');
+                    $('.' + CONTANTER_CLASS).html(result.component.html);
+                    render();
+                });
+            });
+
+        }); 
     };
+
+    function loadWidgetIncludeFiles(items, onSuccess) {
+        console.log('Load widget include files.');
+        var loaded = 0;
+
+        items.forEach(function(item) {
+            var url = REMOTE_URL + item              
+            loadScript(url,function() {
+                console.log('Library file loaded: ' + url);
+                loaded++;
+                if (loaded => items.length) {
+                    onSuccess(loaded);
+                    return;
+                }
+            });                         
+        }); 
+    }
+
+    function loadWidgetFiles(items, onSuccess) {
+        console.log('Load widget files.');
+        var loaded = 0;
+
+        items.forEach(function(item) {
+            var url = ARIKAIM_HOST + item.url             
+            loadScript(url,function() {
+                console.log('Widget component file loaded: ' + url);
+                loaded++;
+                if (loaded => items.length) {
+                    onSuccess(loaded);
+                    return;
+                }
+            });                         
+        }); 
+    }
 
 })();
